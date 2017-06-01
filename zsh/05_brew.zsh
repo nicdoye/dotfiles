6f3c5a89674da9b342ca645d66cdfdcd773ae0f7
Nic Doye <nic.doye@alfresco.com>
2017-06-01 11:52:27 +0100

    Moved setmyokta to ~/unix/bin which was missing from path

--- zsh/05_brew.zsh
+++ zsh/05_brew.zsh
@@ -3,7 +3,5 @@
 # Sets brew_prefix to output of the anonymous function
 #local brew_prefix=$(brew --prefix)
 brew_prefix=$(brew --prefix)
-# Non-Homebrew
-local opt_prefix=${HOME}/unix/opt
 ########################################################################
 export PATH=/usr/local/sbin:${PATH}
