5dd63b3ab27178a48d42f3983119bf21e0f37cb4
Nic Doye <nic.doye@alfresco.com>
2017-07-14 11:54:10 +0100

    DOTFILES renamed

--- zsh/90_antibody.zsh
+++ zsh/90_antibody.zsh
@@ -1,4 +1,4 @@
 source <(antibody init)
-antibody bundle < "$DOTFILES/antibody/bundles.txt" > ~/.zsh_plugins.sh
+antibody bundle < "${dotfiles_dir}/antibody/bundles.txt" > ~/.zsh_plugins.sh
 #source ~/.zsh_plugins.sh
 #antibody update
