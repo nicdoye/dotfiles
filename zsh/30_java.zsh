e6bf0f509e40a46f82400b0f53317ea10ea7a32f
Nic Doye <nic.doye@alfresco.com>
2017-09-15 15:30:50 +0100

    Migrate to sdkman

--- zsh/30_java.zsh
+++ zsh/30_java.zsh
@@ -1,3 +1,1 @@
-   # JAVA
-    export JAVA_HOME=$(/usr/libexec/java_home)
-    export GROOVY_HOME=${brew_prefix}/opt/groovy/libexec
+  export JAVA_HOME=$(/usr/libexec/java_home)
---
+++ zsh/99_sdkman.zsh
@@ -0,0 +1,3 @@
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/Users/ndoye/.sdkman"
+[[ -s "/Users/ndoye/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/ndoye/.sdkman/bin/sdkman-init.sh"
