d6e132d5609ba780a4d518b7975d03271453fdc0
Nic Doye <nic.doye@alfresco.com>
2018-03-14 16:51:18 +0000

    Local go builds

--- zsh/20_go.zsh
+++ zsh/20_go.zsh
@@ -1,4 +1,4 @@
-    # Go
-    export GOPATH=${HOME}/go
-    export GOROOT=${brew_prefix}/opt/go/libexec
-    export PATH=${PATH}:${GOPATH}/bin:${GOROOT}/bin
+# Go
+export GOPATH=${HOME}/go
+export GOROOT=${brew_prefix}/opt/go/libexec
+export PATH=${PATH}:${GOPATH}/bin:${GOROOT}/bin
---
+++ zsh/25_go_builds.zsh
@@ -0,0 +1,2 @@
+export PATH=${GOPATH}/src/github.com/Azure/brigadei/bin:${PATH}
+export PATH=${GOPATH}/src/k8s.io/helm/bin:${PATH}
