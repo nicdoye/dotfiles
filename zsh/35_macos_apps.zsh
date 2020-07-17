0f3a84b5c76e22427f1182bf4c1c6764207279ab
Nic Doye <nic.doye@alfresco.com>
2020-07-17 11:27:17 +0100

    Change to alias

--- zsh/35_macos_apps.zsh
+++ zsh/35_macos_apps.zsh
@@ -4,4 +4,5 @@
 # VS Code
 # PATH+=":/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 # VS Codium
-PATH+=":/Applications/VSCodium.app/Contents/Resources/app/bin"
+# PATH+=":/Applications/VSCodium.app/Contents/Resources/app/bin"
+alias code=codium
