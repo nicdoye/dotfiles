27e95fc58fabab8c10f3a32bc2bce269553c2415
Nic Doye <nic.doye@alfresco.com>
2020-07-17 10:53:04 +0100

    VS Codium

--- zsh/35_macos_apps.zsh
+++ zsh/35_macos_apps.zsh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
 # Allows you to type "code ."
-PATH+=":/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+# VS Code
+# PATH+=":/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+# VS Codium
+PATH+=":/Applications/VSCodium.app/Contents/Resources/app/bin"
