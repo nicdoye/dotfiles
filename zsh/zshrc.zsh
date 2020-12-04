1b56a795d4cc7937a2bbc216434883ef2a8f57c7
Nic Doye <nic.doye@alfresco.com>
2020-12-04 17:14:23 +0000

    simplify

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,24 +6,10 @@
 () {
     # Run this first. Not at the end or you put /usr/local/bin etc. in front of
     # rbenv et al.
-    if test -f /usr/libexec/path_helper; then
-        eval $(/usr/libexec/path_helper -s)
-    fi
 
-    export dotfiles_dir="${HOME}/.dotfiles"
+    local _bsd_ph=/usr/libexec/path_helper
+    [ -x ${_bsd_ph} ] && eval $(${_bsd_ph} -s)
 
-    # Utility function - unused
-    includer () {
-        local filename=$1
-        # Removing error, because kubectl one returns 127 o_O
-        # [ -f "${filename}" ] && source "${filename}" || echo "Error with or in ${filename}"
-        [ -f "${filename}" ] && source "${filename}"
-    }
-
-    local secret_zshrc="${HOME}/.secrets/secrets.zsh"
-    for file in "${secret_zshrc}" ${dotfiles_dir}/zsh/<00-99>_*.zsh
-    do
-        includer ${file}
-    done
+    source ${HOME}/.secrets/secrets.zsh ${HOME}/.dotfiles/zsh/<00-99>_*.zsh
 }
 
