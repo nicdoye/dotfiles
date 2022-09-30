a0d07564a386d1b6d9797819c197604991a268f8
Nic Doye <nic.doye@hyland.com>
2022-09-30 10:59:20 +0100

    brew bin not near beginning. Old Python. Spaceship changes

--- zsh/05_brew.zsh
+++ zsh/05_brew.zsh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
+# Actually does bin and sbin. I want bin before /usr/bin in my path.
 brew::sbin::dir () {
     if type brew &>> /dev/null; then
         brew_prefix=$(brew --prefix)
-        export PATH=${brew_prefix}/sbin:${PATH}
+        export PATH=${brew_prefix}/bin:${brew_prefix}/sbin:${PATH}
         # https://github.com/Homebrew/brew/pull/10374
         export HOMEBREW_BOOTSNAP=1
     fi
