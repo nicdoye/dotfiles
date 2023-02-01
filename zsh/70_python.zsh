b1dd3fa0a914e2d990032728bba03c4a424a6a9f
Nic Doye <nic.doye@hyland.com>
2023-02-01 15:19:24 +0000

    70 Python added

---
+++ zsh/70_python.zsh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+python_version=$(python3 -c 'import sys;print("{}.{}".format(sys.version_info.major,sys.version_info.minor))')
+export PATH="${HOME}/Library/Python/${python_version}/bin":$PATH
