bde77b1947194b7c04a064655b03ccdcc03acf4b
Nic Doye <nic.doye@hyland.com>
2023-04-03 11:25:45 +0100

    guard around homebrew

--- zsh/05_brew.zsh
+++ zsh/05_brew.zsh
@@ -3,9 +3,11 @@
 export HOMEBREW_BOOTSNAP=1
 export HOMEBREW_NO_ENV_HINTS=1
 
-# Assume brew is in here
-eval "$(/opt/homebrew/bin/brew shellenv)"
+brew_executable=/opt/homebrew/bin/brew
+if test -f "${brew_executable}"; then
+    # Assume brew is in here
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# But then calculate it for other files ¯\_(ツ)_/¯
-brew_prefix=$(brew --prefix)
-
+    # But then calculate it for other files ¯\_(ツ)_/¯
+    brew_prefix=$(brew --prefix)
+fi
