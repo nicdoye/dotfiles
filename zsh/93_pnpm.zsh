57c4c15c6315ea65ece7e84371a908ea5893af2e
Nic Doye <nic@worldofnic.org>
2026-09-15 09:39:46 +0100

    pnpm

---
+++ zsh/93_pnpm.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# pnpm
+export PNPM_HOME='${HOME}/Library/pnpm'
+
+if [ -d "$PNPM_HOME" ]; then
+    case ":$PATH:" in
+        *":$PNPM_HOME/bin:"*) ;;
+        *) export PATH="$PNPM_HOME/bin:$PATH" ;;
+    esac
+fi
+# pnpm end
